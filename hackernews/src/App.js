8c4788c42d196dcd2ca7e0842864059864f7a198
Felix Michaux <f.michaux@rcvs.org.uk>
2018-10-09 16:59:19 +0100

    Page 19

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -12,23 +12,23 @@ const list = [
   },
   {
     title: 'Redux',
-    url: '',
-    author: '',
-    num_comments: 0,
-    points: 0,
-    objectID: 0,
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   }
 ]
 
 class App extends Component {
   render() {
-    var myName = {
-      first: 'Gareth',
-      last: 'Perkins'
-    }
     return (
       <div className="App">
-        <h2>Hi there, Mr {myName.first} {myName.last}</h2>
+        {
+          list.map(function(item) {
+            return <div>{item.title}</div>
+          })
+        }
       </div>
     );
   }
