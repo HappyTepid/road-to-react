157842a69f8058c4ec8531be04d5c35cff5f0578
Felix Michaux <felix@michaux.co>
2018-10-09 21:46:45 +0100

    page 21

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -26,7 +26,16 @@ class App extends Component {
       <div className="App">
         {
           list.map(function(item) {
-            return <div>{item.title}</div>
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            )
           })
         }
       </div>
