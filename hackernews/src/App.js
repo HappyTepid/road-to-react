2921a89e8b04b5556392c258b69bb86730bc080d
Felix Michaux <felix@michaux.co>
2018-10-14 15:14:53 +0100

    Page 99

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -6,6 +6,7 @@ const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
 
 
 class App extends Component {
@@ -26,11 +27,24 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error)
@@ -62,7 +76,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
+    const page = (result && result.page) || 0;
     if (!result) { return null; }
 
     return (
@@ -81,6 +95,11 @@ class App extends Component {
               onDismiss={this.onDismiss}
             />
           }
+          <div className="interactions">
+            <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+              More
+          </Button>
+          </div>
         </div>
       </div>
     );
